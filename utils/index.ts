d7a392b610f33bec7a41c33a6da755f372642050
Priyanshsharma21 <piyuindia4@gmail.com>
2023-03-30 16:09:01 +0530

    v2 again

--- utils/index.ts
+++ utils/index.ts
@@ -1,27 +1,4 @@
-// import axios from 'axios';
 
-// export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-// export const createOrGetUser = async (response: any, addUser: any) => {
-//   var base64Url = response.credential.split('.')[1];
-//   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-//   var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-//     return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-//   }).join(''));
-  
-//   const { name, picture, sub } = JSON.parse(jsonPayload)
-  
-//   const user = {
-//     _id: sub,
-//     _type: 'user',
-//     userName: name,
-//     image: picture,
-//   };
-  
-//   addUser(user);
-
-//   await axios.post(`${BASE_URL}/api/auth`, user);
-// };
 
 import axios from "axios";
 import jwt_decode from 'jwt-decode'
@@ -42,7 +19,7 @@ export const createOrGetUser = async (response : any, addUser : any)=>{
 
   addUser(user); // and now when we reload the page user still be there
   
-  await axios.post(`${BASE_URL}/api/auth`, user)
-  // await axios.post(`https://do-tok-app.vercel.app/api/auth`, user)
+  // await axios.post(`${BASE_URL}/api/auth`, user)
+  await axios.post(`https://do-tok-app.vercel.app/api/auth`, user)
   
 }
